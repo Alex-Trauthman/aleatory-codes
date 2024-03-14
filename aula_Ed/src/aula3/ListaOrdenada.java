c29e6c1d143cf5b43f33afaf7e411c06e72093da
Alex-Trauthman <122585413+Alex-Trauthman@users.noreply.github.com>
2024-03-13 21:15:20 -0300

    adsffa

--- aula_Ed/src/aula3/ListaOrdenada.java
+++ aula_Ed/src/aula3/ListaOrdenada.java
@@ -1,49 +1,70 @@
 package aula3;
 
 public class ListaOrdenada {
-	No comeco;
-	
-	ListaOrdenada(){
-		this.comeco=null;
-	}
-	
-	public void inserir(int elemento) {
-		No novoNo = new No(elemento);
-		No temp = comeco;
-		if(temp!=null) {
-			while (temp.getProximo() != null) {
-				if (novoNo.getElemento() == temp.getProximo().getElemento()) {
-					temp.setProximo(null);
-				}else if (novoNo.getElemento()<temp.getElemento()) {
-					novoNo.setProximo(temp);
-					temp = novoNo;
-					temp = temp.getProximo();
-				}else {
-					temp = temp.getProximo();
-				}
-			}
-		}
-		if (temp == null) {
-			temp = novoNo; 
-		}else if (novoNo.getElemento()>=temp.getElemento()) {
-			temp.setProximo(novoNo);
-		}
-		
-		
-		comeco = temp;
-	}
-	
-	public void imprimir() {
-		No temp = comeco;
-        System.out.print("Lista: ");
-        System.out.println(" ");
-        while (temp != null) {
-            System.out.print(temp.getElemento());
-            temp = temp.getProximo();
-            System.out.println(" ");
+    private No comeco;
+
+    ListaOrdenada() {
+        this.comeco = null;
+    }
+
+    public void inserir(int elemento) {
+        No novoNo = new No(elemento);
+        if (comeco == null || elemento < comeco.getElemento()) {
+            novoNo.setProximo(comeco);
+            comeco = novoNo;
+        } else {
+            No temp = comeco;
+            while (temp.getProximo() != null && elemento > temp.getProximo().getElemento()) {
+                temp = temp.getProximo();
+            }
+            novoNo.setProximo(temp.getProximo());
+            temp.setProximo(novoNo);
         }
-	}
-	public static void main(String[] args) {
-		
-	}
+    }
+
+    public void excluir(int elemento) {
+        if (comeco == null) {
+            return; 
+        }
+
+        if (comeco.getElemento() == elemento) {
+            comeco = comeco.getProximo();
+            return;
+        }
+
+        No anterior = comeco;
+        No atual = comeco.getProximo();
+
+        while (atual != null && atual.getElemento() != elemento) {
+            anterior = atual;
+            atual = atual.getProximo();
+        }
+
+        if (atual != null) {
+            anterior.setProximo(atual.getProximo());
+        }
+    }
+
+    public void imprimir() {
+        No temp = comeco;
+        System.out.print("Lista: ");
+        while (temp != null) {
+            System.out.print(temp.getElemento() + " ");
+            temp = temp.getProximo();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListaOrdenada lista = new ListaOrdenada();
+        lista.inserir(5);
+        lista.inserir(3);
+        lista.inserir(8);
+        lista.inserir(2);
+        lista.inserir(7);
+        lista.imprimir();
+
+        lista.excluir(3);
+        lista.imprimir();
+    }
 }
